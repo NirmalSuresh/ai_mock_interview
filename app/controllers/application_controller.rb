3e47df996b18fab360ca8e5a7b8a90d27fce9736
Nirmal Suresh <nirmalsuresh16@gmail.com>
2025-11-25 10:41:32 +0000

    Fix user association + login redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF issues
-  protect_from_forgery with: :exception
-
-  # Where to go after login
   def after_sign_in_path_for(resource)
     new_interview_path
   end
