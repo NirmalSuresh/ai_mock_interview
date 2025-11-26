139a1c4dc599f5530ebcf29296c00de3b183c1e1
Nirmal Suresh <nirmalsuresh16@gmail.com>
2025-11-26 21:17:53 +0000

    changes made

--- app/javascript/controllers/autoscroll_controller.js
+++ app/javascript/controllers/autoscroll_controller.js
@@ -1,6 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  connect() { this.scroll() }
-  scroll() { this.element.scrollTop = this.element.scrollHeight }
+  connect() {
+    this.scroll();
+  }
+
+  scroll() {
+    this.element.scrollTop = this.element.scrollHeight;
+  }
+
+  update() {
+    this.scroll();
+  }
 }
