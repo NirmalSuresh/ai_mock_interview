38a6d68fc2661fceaf34c99846a6c4a0b059cee9
Nirmal Suresh <nirmalsuresh16@gmail.com>
2025-11-27 00:03:20 +0000

    Fix importmap Stimulus path

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,12 +1,6 @@
-# config/importmap.rb
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
-pin "controllers", to: "controllers/index.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-
-pin "@popperjs/core", to: "popper.js"
-pin "bootstrap", to: "bootstrap.min.js"
