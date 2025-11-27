95b027aa9c4caad8148d04b905fdcc99ad3ea360
Nirmal Suresh <nirmalsuresh16@gmail.com>
2025-11-27 06:55:16 +0000

    Remove Popper & Bootstrap JS, fix Stimulus

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,3 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
-
-# Bootstrap + PopperJS
-pin "@popperjs/core", to: "popper.js"
-pin "bootstrap", to: "bootstrap.min.js"
