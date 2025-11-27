47e0dd6a850371522f7538b655ded35af13575b2
Nirmal Suresh <nirmalsuresh16@gmail.com>
2025-11-27 06:50:21 +0000

    Fix popper import

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# Bootstrap + PopperJS (correct fingerprinted asset)
-pin "@popperjs/core", to: "popper-003a40d80fd205e1fa00da117d5bdc19720ba330706eaa17f9ba9513fa502304.js"
+# Bootstrap + PopperJS
+pin "@popperjs/core", to: "popper.js"
 pin "bootstrap", to: "bootstrap.min.js"
