7a2d7ad6e041a17539b8e8d478a58d4f7da441b3
Nirmal Suresh <nirmalsuresh16@gmail.com>
2025-11-27 06:30:08 +0000

    fix popper + stimulus + bootstrap load

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,10 +3,9 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
+# Load all Stimulus controllers automatically
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# CORRECT POPPERJS FROM PUBLIC/ASSETS
-pin "@popperjs/core", to: "popper-003a40d80fd205e1fa00da117d5bdc19720ba330706eaa17f9ba9513fa502304.js"
-
-# CORRECT BOOTSTRAP FROM PUBLIC/ASSETS
-pin "bootstrap", to: "bootstrap.min-4ecafa8f279d0b285b3d27ca02c7e5da187907efe2c38f83eb8b4c7d6aa151c4.js"
+# Bootstrap JS support
+pin "@popperjs/core", to: "popper.js"
+pin "bootstrap", to: "bootstrap.min.js"
